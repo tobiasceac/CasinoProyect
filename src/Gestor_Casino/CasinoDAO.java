e162165fac9c16a29ec2c9c15bed96af5ef8c431
tobias <tobiasceniceros@gmail.com>
2025-10-22 10:33:23 +0200

    Initial commit

---
+++ src/Gestor_Casino/CasinoDAO.java
@@ -0,0 +1,50 @@
+package Gestor_Casino;
+
+public interface CasinoDAO {
+    //Metodos CRUD
+
+    /**
+     * Crea un objeto usuario
+     */
+    public void crearUsuario();
+
+    /**
+     * Crea un objeto Gestor_Casino.Mesa
+     */
+
+    public void crearMesa();
+
+
+
+    public String consultaMesa();
+
+    public String consultaUsuario();
+
+    public boolean actualizarMesa();
+
+    public boolean actualizarUsuario();
+
+    public boolean borrarMesa();
+
+    public boolean borrarUsuario();
+
+
+
+    //Metodos no CRUD
+
+    /**
+     * Devuelve el valor de
+     * @return
+     */
+    public float balancePorMesa();
+
+    /**
+     *
+     * @return
+     */
+    public float dineroInvertidoAlimentos();
+
+    public float dineroInvertido();
+
+    public int vecesJugadasMesa();
+}
