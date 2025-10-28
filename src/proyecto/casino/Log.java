72b9930118302ae7fede8492b4a7f3472ff241e0
mariosinnadamas <mariosaeztellez@gmail.com>
2025-10-28 11:00:52 +0100

    Actualización, avance en las clases Log, Servicio, TipoConcepto, TipoServicio y Cliente, 28/10 11:00

--- src/proyecto/casino/Log.java
+++ src/proyecto/casino/Log.java
@@ -4,10 +4,109 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Log {
-    private String dni;
-    private String codigo;
+    private Cliente cliente;
+    private Servicio servicio;
     private LocalDate fecha;
     private LocalTime hora;
     private TipoConcepto concepto;
-    private double gananciaCasino;
+    private double cantidadConcepto;
+
+    //Constructor vacío para JSON (Serialización)
+    public Log() {
+    }
+
+    //Constructor sin fecha y hora para crear log
+    public Log(Cliente cliente, Servicio servicio, TipoConcepto concepto, double cantidadConcepto) {
+        setCliente(cliente);
+        setServicio(servicio);
+        setConcepto(concepto);
+        setCantidadConcepto(cantidadConcepto);
+        this.fecha = LocalDate.now();
+        this.hora = LocalTime.now();
+    }
+
+    //Igual es necesario un constructor completo para la lectura del JSON/XML
+    public Log(Cliente cliente, Servicio servicio, LocalDate fecha, LocalTime hora, TipoConcepto concepto, double cantidadConcepto) {
+        setCliente(cliente);
+        setServicio(servicio);
+        setConcepto(concepto);
+        setCantidadConcepto(cantidadConcepto);
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        if (cliente == null){
+            throw new IllegalArgumentException("Cliente no puede ser null");
+        }
+        this.cliente = cliente;
+    }
+
+    public Servicio getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(Servicio servicio) {
+        if (servicio == null){
+            throw new IllegalArgumentException("Servicio no puede ser null");
+        }
+        this.servicio = servicio;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        if (fecha.isAfter(LocalDate.now())){
+            throw new IllegalArgumentException("La fecha no puede ser futura");
+        }
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public TipoConcepto getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(TipoConcepto concepto) {
+        if (concepto == null){
+            throw new IllegalArgumentException("Concepto no puede ser null");
+        }
+        this.concepto = concepto;
+    }
+
+    public double getCantidadConcepto() {
+        return cantidadConcepto;
+    }
+
+    public void setCantidadConcepto(double cantidadConcepto) {
+        if (cantidadConcepto <= 0){
+            throw new IllegalArgumentException("La cantidad debe ser mayor a 0");
+        }
+        this.cantidadConcepto = cantidadConcepto;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "cliente=" + cliente +
+                ", servicio=" + servicio +
+                ", fecha=" + fecha +
+                ", hora=" + hora +
+                ", concepto=" + concepto +
+                ", cantidadConcepto=" + cantidadConcepto +
+                '}';
+    }
 }
